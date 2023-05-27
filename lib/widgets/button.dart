51cb0ae106b72521856405c6a0c5df6e8f0875c0
White-Patrasche <5338095@gmail.com>
2023-05-27 20:22:13 +0900

    feat: review button

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:may_be_clean/consts/consts.dart';
 
-class CategoryButton extends StatefulWidget {
+class CategoryButton extends StatelessWidget {
   final String title;
   final String selectedSvg;
   final String unselectedSvg;
   final bool isSelected;
+  final double fontSize;
   final Function() action;
 
   const CategoryButton({
@@ -15,24 +16,20 @@ class CategoryButton extends StatefulWidget {
     required this.unselectedSvg,
     required this.isSelected,
     required this.action,
+    this.fontSize = 12,
     super.key,
   });
 
   @override
-  State<CategoryButton> createState() => _CategoryButtonState();
-}
-
-class _CategoryButtonState extends State<CategoryButton> {
-  @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: widget.action,
+      onTap: action,
       child: Container(
           padding: const EdgeInsets.fromLTRB(10, 7, 10, 7),
           margin: const EdgeInsets.all(5),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(50),
-            color: (widget.isSelected) ? ColorSystem.primary : Colors.white,
+            color: (isSelected) ? ColorSystem.primary : Colors.white,
             boxShadow: [
               BoxShadow(
                 color: Colors.grey.withOpacity(0.3),
@@ -44,16 +41,57 @@ class _CategoryButtonState extends State<CategoryButton> {
           child: Row(
             children: [
               SvgPicture.asset(
-                (widget.isSelected) ? widget.selectedSvg : widget.unselectedSvg,
+                (isSelected) ? selectedSvg : unselectedSvg,
                 width: 16,
               ),
               const SizedBox(width: 2),
-              Text(widget.title,
-                  style: FontSystem.body2.copyWith(
-                    color: (widget.isSelected) ? Colors.white : Colors.black,
+              Text(title,
+                  style: FontSystem.caption.copyWith(
+                    color: (isSelected) ? Colors.white : Colors.black,
+                    fontSize: fontSize,
                   )),
             ],
           )),
+    );
+  }
+}
+
+class ReviewButton extends StatelessWidget {
+  final String title;
+  final String image;
+  final Function() action;
+
+  const ReviewButton({
+    super.key,
+    required this.title,
+    required this.image,
+    required this.action,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: action,
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(5, 3, 5, 3),
+        margin: const EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: ColorSystem.gray3,
+          border: Border.all(width: 0.5, color: ColorSystem.gray2),
+        ),
+        child: Row(
+          children: [
+            Image.asset(
+              image,
+              width: 16,
+            ),
+            const SizedBox(width: 2),
+            Text(title,
+                style: FontSystem.caption.copyWith(color: Colors.black)),
+          ],
+        ),
+      ),
     );
   }
 }
