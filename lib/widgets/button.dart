ed93e3d526677aa1ba03e13e801a27864e00a56c
ChoiSangwon <fdsa200@naver.com>
2023-05-29 18:38:34 +0900

    refactor : button.dart width 변수로 전달

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:may_be_clean/consts/consts.dart';
 
-class CategoryButton extends StatelessWidget {
+class CategoryButton extends StatefulWidget {
   final String title;
   final String selectedSvg;
   final String unselectedSvg;
@@ -23,74 +23,40 @@ class CategoryButton extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: action,
-      child: Container(
-          padding: const EdgeInsets.fromLTRB(10, 7, 10, 7),
-          margin: const EdgeInsets.all(5),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(50),
-            color: (isSelected) ? ColorSystem.primary : Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.3),
-                blurRadius: 1,
-                spreadRadius: 2,
-              ),
-            ],
-          ),
-          child: Row(
-            children: [
-              SvgPicture.asset(
-                (isSelected) ? selectedSvg : unselectedSvg,
-                width: 16,
-              ),
-              const SizedBox(width: 2),
-              Text(title,
-                  style: FontSystem.caption.copyWith(
-                    color: (isSelected) ? Colors.white : Colors.black,
-                    fontSize: fontSize,
-                  )),
-            ],
-          )),
-    );
-  }
+  State<CategoryButton> createState() => _CategoryButtonState();
 }
 
-class ReviewButton extends StatelessWidget {
-  final String title;
-  final String image;
-  final Function() action;
-
-  const ReviewButton({
-    super.key,
-    required this.title,
-    required this.image,
-    required this.action,
-  });
-
+class _CategoryButtonState extends State<CategoryButton> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: action,
+      onTap: widget.action,
       child: Container(
-        padding: const EdgeInsets.fromLTRB(5, 3, 5, 3),
+        padding: const EdgeInsets.fromLTRB(10, 7, 10, 7),
         margin: const EdgeInsets.all(5),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(50),
-          color: ColorSystem.gray3,
-          border: Border.all(width: 0.5, color: ColorSystem.gray2),
+          color: (widget.isSelected) ? ColorSystem.primary : Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              blurRadius: 1,
+              spreadRadius: 2,
+            ),
+          ],
         ),
         child: Row(
           children: [
-            Image.asset(
-              image,
-              width: 16,
+            SvgPicture.asset(
+              (widget.isSelected) ? widget.selectedSvg : widget.unselectedSvg,
+              width: widget.width,
             ),
             const SizedBox(width: 2),
-            Text(title,
-                style: FontSystem.caption.copyWith(color: Colors.black)),
+            Text(widget.title,
+                style: FontSystem.body2.copyWith(
+                  fontSize: widget.fontSize,
+                  color: (widget.isSelected) ? Colors.white : Colors.black,
+                )),
           ],
         ),
       ),
