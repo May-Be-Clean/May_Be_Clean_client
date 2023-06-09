285d3f362e620d5f6062cf2b4b75c8f053f5df09
White-Patrasche <5338095@gmail.com>
2023-06-09 14:09:41 +0900

    feat: init myreviewscreen

---
+++ lib/screens/mypage/myreview_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:may_be_clean/widgets/widgets.dart';
+
+class MyReviewScreen extends StatefulWidget {
+  const MyReviewScreen({super.key});
+
+  @override
+  State<MyReviewScreen> createState() => _MyReviewScreenState();
+}
+
+class _MyReviewScreenState extends State<MyReviewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(title: '내가 작성한 후기'),
+    );
+  }
+}
