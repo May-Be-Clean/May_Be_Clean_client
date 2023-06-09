1bfdc89cef4d64b923abe2f5bb4bc5a761229fd2
White-Patrasche <5338095@gmail.com>
2023-06-09 14:36:30 +0900

    feat: auto login

--- lib/states/global.dart
+++ lib/states/global.dart
@@ -1,31 +1,62 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:may_be_clean/models/model.dart';
+import 'package:may_be_clean/screens.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class GlobalState extends GetxController {
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
   RxMap<String, Marker> markers = RxMap<String, Marker>({});
-  String token = "";
-  String trashType1 = '';
-  String trashType2 = '';
+  String token = "test";
   late TabController tabController;
   User? user;
 
   Future<void> load() async {
-    Timer(const Duration(milliseconds: 500), () async {});
+    Timer(const Duration(milliseconds: 1000), () async {
+      Get.to(() => const HomeScreen());
+    });
+    // try {
+    //   final prefs = await SharedPreferences.getInstance();
+    //   Timer(const Duration(milliseconds: 1000), () async {
+    //     final token = prefs.getString('accessToken');
+    //     // if (token == null) {
+    //     //   Get.to(() => const LoginScreen());
+    //     //   return;
+    //     // }
+    //     // if (token == "") {
+    //     //   Get.to(() => const HomeScreen());
+    //     //   return;
+    //     // }
+    //     final autoLoginUser = await User.getUser(this.token);
+    //     login(autoLoginUser);
+    //     Get.to(() => const HomeScreen());
+    //   });
+    // } catch (e, s) {
+    //   log(e.toString(), stackTrace: s);
+    //   Get.to(() => const HomeScreen());
+    // }
+  }
+
+  void login(User user) {
+    this.user = user;
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.setString('accessToken', user.accessToken ?? '');
+    });
+  }
+
+  void logout() {
+    user = null;
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.remove('accessToken');
+    });
+    update();
   }
 
   @override
   void onInit() async {
     super.onInit();
-    user = User(
-      id: 124,
-      name: '유저 이름',
-      exp: 14,
-    );
     await load();
   }
 }
