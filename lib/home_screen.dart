1fd0d07984da5db52dd563444b8d6d9f67037935
ChoiSangwon <fdsa200@naver.com>
2023-06-09 22:34:18 +0900

    refactor : 로그인 후 찜 클릭 시 바로 넘어가기

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -55,7 +55,7 @@ class _HomeScreen extends State<HomeScreen> with TickerProviderStateMixin {
 
     if (index == _index) return;
 
-    if (index == 2 && _globalStates.user == null) {
+    if (index == 2 && _globalStates.userData == null) {
       loginRequest(context);
       return;
     }
