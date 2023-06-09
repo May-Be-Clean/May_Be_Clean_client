72518d7e238d6f0bf0b7c059d711c52504094cd4
White-Patrasche <5338095@gmail.com>
2023-06-09 14:32:57 +0900

    feat: add empty data set

--- lib/states/store.dart
+++ lib/states/store.dart
@@ -1,32 +1,22 @@
 import 'package:get/get.dart';
 import 'dart:async';
 import 'package:may_be_clean/models/model.dart';
+import 'package:may_be_clean/states/states.dart';
 
 class StoreState extends GetxController {
   List<Store> stores = [];
+  final _globalStates = Get.find<GlobalState>();
 
-  Future<void> load() async {
-    for (int i = 0; i < 7; i++) {
-      stores.add(Store(
-        id: 4,
-        name: '덕분애 제로웨이스트샵',
-        address1: '서울 서초구 서운로26길 11 2층',
-        address2: '서울 서초구 서초동 1337-1',
-        latitude: 37.486,
-        longitude: 127.019,
-        phone: '02-6959-4479',
-        category: ['vegan', 'cafe'],
-        openTime: '1200',
-        cloverCount: 2,
-        closeTime: '2000',
-        updatedAt: DateTime.now(),
-      ));
-    }
+  Future loadMarker(double upperLat, double upperLon, double lowerLat,
+      double lowerLon, List<String> categories) async {
+    final result = await Store.getNearbyStore(_globalStates.token, upperLat,
+        upperLon, lowerLat, lowerLon, categories);
+    result.addAll(result);
   }
 
   @override
   void onInit() async {
     super.onInit();
-    await load();
+    stores.add(emptyStoreData);
   }
 }
