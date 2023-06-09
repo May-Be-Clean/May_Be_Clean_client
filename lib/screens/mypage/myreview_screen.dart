3e6d9b0e3eb39276719264c66c534a01b1f170d4
White-Patrasche <5338095@gmail.com>
2023-06-09 14:38:30 +0900

    feat: component styling

--- lib/screens/mypage/myreview_screen.dart
+++ lib/screens/mypage/myreview_screen.dart
@@ -11,7 +11,7 @@ class MyReviewScreen extends StatefulWidget {
 class _MyReviewScreenState extends State<MyReviewScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       appBar: CustomAppBar(title: '내가 작성한 후기'),
     );
   }
