81b8004135d179bce90aa167aba854c9cd1a305c
White-Patrasche <5338095@gmail.com>
2023-06-09 14:31:44 +0900

    feat: update models by server DTO

--- lib/models/review.dart
+++ lib/models/review.dart
@@ -1,39 +1,122 @@
-import 'package:may_be_clean/models/model.dart';
-
 class Review {
-  final int id;
+  final int reviewId;
+  final int userId;
+  final String nickname;
+  final int point;
+  final int storeId;
   final String storeName;
-  final User user;
-  final String contents;
-  final int cloverCount;
-  final List<String> storeCategory;
-  final List<String> reviewCategory;
-  final List<String> images;
+  final List<String> storeCategories;
+  final int clover;
+  final String content;
+  final List<String> imageUrls;
+  final List<String> reviewCategories;
+  final ReviewFilterCount reviewFilterCount;
   final DateTime createdAt;
 
   Review({
-    required this.id,
+    required this.reviewId,
+    required this.userId,
+    required this.nickname,
+    required this.point,
+    required this.storeId,
     required this.storeName,
-    required this.user,
-    required this.contents,
-    required this.cloverCount,
-    required this.storeCategory,
-    required this.reviewCategory,
-    required this.images,
+    required this.storeCategories,
+    required this.clover,
+    required this.content,
+    required this.imageUrls,
+    required this.reviewCategories,
     required this.createdAt,
+    required this.reviewFilterCount,
   });
 
   factory Review.fromJson(Map<String, dynamic> json) {
     return Review(
-      id: json['id'],
+      reviewId: json['reviewId'],
+      userId: json['userId'],
+      nickname: json['nickname'],
+      point: json['point'],
+      storeId: json['storeId'],
       storeName: json['storeName'],
-      user: User.fromJson(json['user']),
-      contents: json['contents'],
-      cloverCount: json['cloverCount'],
-      storeCategory: List<String>.from(json['storeCategory']),
-      reviewCategory: List<String>.from(json['reviewCategory']),
-      images: List<String>.from(json['images']),
+      storeCategories: json['storeCategories'].cast<String>(),
+      clover: json['clover'],
+      content: json['content'],
+      imageUrls: json['imageUrls'].cast<String>(),
+      reviewCategories: json['reviewCategories'].cast<String>(),
       createdAt: DateTime.parse(json['createdAt']),
+      reviewFilterCount: ReviewFilterCount.fromJson(json['reviewFilterCount']),
     );
   }
 }
+
+class ReviewFilterCount {
+  final int clean;
+  final int large;
+  final int parking;
+  final int mood;
+  final int variant;
+  final int valuable;
+  final int quality;
+  final int effective;
+  final int kind;
+
+  ReviewFilterCount({
+    required this.clean,
+    required this.large,
+    required this.parking,
+    required this.mood,
+    required this.variant,
+    required this.valuable,
+    required this.quality,
+    required this.effective,
+    required this.kind,
+  });
+
+  factory ReviewFilterCount.fromJson(Map<String, dynamic> json) {
+    return ReviewFilterCount(
+      clean: json['countOfCleanStore'],
+      large: json['countOfLargeStore'],
+      parking: json['countOfParking'],
+      mood: json['countOfMood'],
+      variant: json['countOfVariant'],
+      valuable: json['countOfValuable'],
+      quality: json['countOfQuality'],
+      effective: json['countOfPrice'],
+      kind: json['countOfKindness'],
+    );
+  }
+}
+
+final emptyReviewData = Review(
+  reviewId: 0,
+  userId: 0,
+  nickname: '',
+  storeId: 0,
+  point: 0,
+  storeName: "덕분애",
+  storeCategories: ['UPCYCLE', 'CAFE'],
+  content: '',
+  imageUrls: [
+    'https://play-lh.googleusercontent.com/kgl_WvLEG-FjkVMJQmw8oWw8-PAKuy2BFHjCQ9pcGRL3S2Yk6usC-h5Cjn7Efkyq2-I',
+    'https://play-lh.googleusercontent.com/kgl_WvLEG-FjkVMJQmw8oWw8-PAKuy2BFHjCQ9pcGRL3S2Yk6usC-h5Cjn7Efkyq2-I'
+  ],
+  clover: 4,
+  reviewCategories: [
+    'CLEAN',
+    'LARGE',
+    'PARKING',
+    'MOOD',
+    'VARIANT',
+  ],
+  reviewFilterCount: ReviewFilterCount(
+    clean: 0,
+    large: 0,
+    parking: 0,
+    mood: 0,
+    variant: 0,
+    valuable: 0,
+    quality: 0,
+    effective: 0,
+    kind: 0,
+  ),
+  createdAt: DateTime.now(),
+);
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,25 +1,84 @@
+import 'package:http/http.dart' as http;
+import 'package:may_be_clean/env/env.dart';
+import 'package:may_be_clean/utils/utils.dart';
+import 'dart:convert';
+
 class User {
-  final int id;
-  final String? email;
-  final String name;
-  final String? profileUrl;
-  final int exp;
+  final String email;
+  final String nickname;
+  final String? accessToken;
+  final String? role;
+  final int point;
+  final int? reviewCount;
+  final int? registerCount;
+  final int? verifiedCount;
 
   User({
-    required this.id,
-    required this.name,
-    this.email,
-    this.profileUrl,
-    required this.exp,
+    required this.email,
+    required this.nickname,
+    this.accessToken,
+    required this.point,
+    this.role,
+    this.reviewCount,
+    this.registerCount,
+    this.verifiedCount,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      id: json['id'],
-      name: json['name'],
+      nickname: json['nickname'],
       email: json['email'],
-      profileUrl: json['profileUrl'],
-      exp: json['exp'],
+      point: json['point'],
+      accessToken: json['accessToken'],
+      role: json['role'],
+      reviewCount: json['reviewCount'],
+      registerCount: json['registerCount'],
+      verifiedCount: json['verifiedCount'],
     );
+  }
+
+  static Future<User> authKakao(String token) async {
+    const api = '${ENV.apiEndpoint}/auth/kakao';
+
+    final response = await http.post(
+      Uri.parse(api),
+      headers: {'Authorization': token},
+    );
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return User.fromJson(json.decode(response.body)['user']);
+    } else {
+      throw newHTTPException(response.statusCode, response.body);
+    }
+  }
+
+  static Future<User> getUser(String token) async {
+    const api = '${ENV.apiEndpoint}/user';
+
+    final response = await http.get(
+      Uri.parse(api),
+      headers: {'Authorization': "Bearer $token"},
+    );
+
+    if (response.statusCode == 200) {
+      return User.fromJson(json.decode(response.body));
+    } else {
+      throw newHTTPException(response.statusCode, response.body);
+    }
+  }
+
+  Future<User> getMypageData(String token) async {
+    const api = '${ENV.apiEndpoint}/user/mypage';
+
+    final response = await http.get(
+      Uri.parse(api),
+      headers: {'Authorization': "Bearer $token"},
+    );
+
+    if (response.statusCode == 200) {
+      return User.fromJson(json.decode(response.body));
+    } else {
+      throw newHTTPException(response.statusCode, response.body);
+    }
   }
 }
