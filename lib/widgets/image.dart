aa5ddf181332af0c3105696e1176656eec8c956c
White-Patrasche <5338095@gmail.com>
2023-06-09 14:10:51 +0900

    refactor: rounded image with image path

--- lib/widgets/image.dart
+++ lib/widgets/image.dart
@@ -2,45 +2,106 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:may_be_clean/consts/consts.dart';
 
+import 'dart:io';
+
 class RoundedImage extends StatelessWidget {
   final String imageUrl;
   final double diameter;
+  final Function()? onTap;
+  final Function()? dismissFunction;
 
-  const RoundedImage({required this.imageUrl, this.diameter = 100, super.key});
+  const RoundedImage(
+      {required this.imageUrl,
+      this.diameter = 100,
+      this.onTap,
+      this.dismissFunction,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CachedNetworkImage(
-      imageUrl: imageUrl,
-      cacheKey: imageUrl,
-      width: diameter,
-      memCacheWidth: diameter.toInt(),
-      maxWidthDiskCache: diameter.toInt(),
-      imageBuilder: (context, imageProvider) => Container(
-        width: diameter,
-        height: diameter,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+    if (imageUrl == '') {
+      return GestureDetector(
+        onTap: onTap,
+        child: Container(
+          width: diameter,
+          height: diameter,
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: ColorSystem.gray3),
+          child: const Icon(Icons.camera_alt, color: ColorSystem.primary),
         ),
-      ),
-      placeholder: (context, url) => Container(
+      );
+    }
+    if (imageUrl.startsWith('http')) {
+      return CachedNetworkImage(
+        imageUrl: imageUrl,
+        cacheKey: imageUrl,
         width: diameter,
-        height: diameter,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.grey,
+        memCacheWidth: diameter.toInt(),
+        maxWidthDiskCache: diameter.toInt(),
+        imageBuilder: (context, imageProvider) => Container(
+          width: diameter,
+          height: diameter,
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+          ),
         ),
-      ),
-      errorWidget: (context, url, error) => Container(
-        width: diameter,
-        height: diameter,
-        decoration: BoxDecoration(
-          color: ColorSystem.gray2,
-          borderRadius: BorderRadius.circular(10),
+        placeholder: (context, url) => Container(
+          width: diameter,
+          height: diameter,
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey,
+          ),
         ),
-        child: const Icon(Icons.error),
-      ),
+        errorWidget: (context, url, error) => Container(
+          width: diameter,
+          height: diameter,
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: ColorSystem.gray2,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: const Icon(Icons.error),
+        ),
+      );
+    }
+    return Stack(
+      children: [
+        Container(
+          width: diameter,
+          height: diameter,
+          margin: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              image: FileImage(File(imageUrl)),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        if (dismissFunction != null)
+          Positioned(
+            right: 0,
+            child: GestureDetector(
+              onTap: dismissFunction,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: ColorSystem.primary,
+                ),
+                child: const Icon(
+                  Icons.close,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          )
+      ],
     );
   }
 }
