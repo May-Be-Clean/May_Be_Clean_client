d12dc428c04d74148286565d5b2fd0c19ec5a812
White-Patrasche <5338095@gmail.com>
2023-06-12 11:53:41 +0900

    feat: mypage screens

--- lib/screens/mypage/myregister_screen.dart
+++ lib/screens/mypage/myregister_screen.dart
@@ -3,6 +3,7 @@ import 'package:may_be_clean/screens.dart';
 import 'package:may_be_clean/widgets/widgets.dart';
 import 'package:get/get.dart';
 import 'package:may_be_clean/states/states.dart';
+import 'package:may_be_clean/models/model.dart';
 
 class MyRegisterScreen extends StatefulWidget {
   const MyRegisterScreen({super.key});
@@ -12,11 +13,22 @@ class MyRegisterScreen extends StatefulWidget {
 }
 
 class _MyRegisterScreenState extends State<MyRegisterScreen> {
-  final _storeStates = Get.find<GlobalState>();
+  final _globalStates = Get.find<GlobalState>();
+  final _myRegisterStores = <Store>[];
+  int _page = 0;
 
   @override
   void initState() {
     super.initState();
+    loadMore();
+  }
+
+  Future<void> loadMore() async {
+    final stores =
+        await Store.getRegistredStores(_globalStates.token, _page, 10);
+    _myRegisterStores.addAll(stores);
+    _page++;
+    setState(() {});
   }
 
   @override
@@ -25,9 +37,9 @@ class _MyRegisterScreenState extends State<MyRegisterScreen> {
       appBar: const CustomAppBar(title: "내가 등록한 가게"),
       backgroundColor: Colors.white,
       body: ListView.separated(
-        itemCount: _storeStates.stores.length,
+        itemCount: _myRegisterStores.length,
         itemBuilder: (context, index) {
-          final store = _storeStates.stores.values.toList()[index];
+          final store = _myRegisterStores[index];
           return StoreCard(store);
         },
         separatorBuilder: (context, index) {
